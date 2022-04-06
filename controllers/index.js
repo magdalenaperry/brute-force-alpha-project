d379d84d20180f2f83f9eb7ac2f2a717328e907d
Magdalena Perry <magperry16@gmail.com>
2022-04-06 11:05:34 -0400

    secondary setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api/index');
+const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>CHECK ROUTE!!!</h1>")
-});
 
 module.exports = router;
