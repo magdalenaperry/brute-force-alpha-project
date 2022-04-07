562b7477ce871c70c609e14ff8db7c901cb20ac2
Magdalena Perry <magperry16@gmail.com>
2022-04-07 18:36:29 -0400

    semi successful patient illness family history form

--- public/js/patient-history-form.js
+++ public/js/patient-history-form.js
@@ -2,36 +2,38 @@ const historyFormHandler = async (event) => {
   console.log('hello')
   event.preventDefault();
   const has_cancer = document.querySelector('#cancer').value.trim();
-  const has_asthma = document.querySelector('#asthma').value.trim();
-  const has_cardiac_disease = document.querySelector('#cardiac-disease').value.trim();
-  const has_diabetes = document.querySelector('#diabetes').value.trim();
-  const has_hypertension = document.querySelector('#hypertension').value.trim();
-  const has_psychiatric_disorder = document.querySelector('#psychiatric').value.trim();
-  const has_epilepsy = document.querySelector('#password-signup').value.trim();
+  const has_asthma = document.querySelector('#asthma').specified
+  // const has_cardiac_disease = document.querySelector('#cardiac-disease').value.trim();
+  // const has_diabetes = document.querySelector('#diabetes').value.trim();
+  // const has_hypertension = document.querySelector('#hypertension').value.trim();
+  // const has_psychiatric_disorder = document.querySelector('#psychiatric').value.trim();
+  // const has_epilepsy = document.querySelector('#epilepsy').value.trim();
 
 
-  if (first_name && last_name && username && email && password) {
-    const response = await fetch('/api/users/signup', {
+  // if () {
+    const response = await fetch('/api/history/forms', {
       method: 'POST',
       body: JSON.stringify({
-        username,
-        email,
-        password,
-        first_name,
-        last_name
+        has_cancer,
+        has_asthma
+        // has_cardiac_disease,
+        // has_diabetes,
+        // has_hypertension,
+        // has_psychiatric_disorder,
+        // has_epilepsy
       }),
       headers: {
         'Content-Type': 'application/json'
       },
     });
-
     if (response.ok) {
       document.location.replace('/');
+      console.log(response);
     } else {
-      alert('Failed to sign up.');
+      alert('Failed to add patient history.');
     }
   }
-};
+// };
 
 document
   .querySelector('.family-history-form')
