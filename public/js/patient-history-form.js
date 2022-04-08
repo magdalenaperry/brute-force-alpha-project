7b202a01504b347cd1b768a94a4bd9a802d9bac3
David Aylward <Davidayl14@outlook.com>
2022-04-08 14:25:02 -0400

    fixed signup styling

--- public/js/patient-history-form.js
+++ public/js/patient-history-form.js
@@ -2,7 +2,7 @@ const historyFormHandler = async (event) => {
   console.log('hello')
   event.preventDefault();
   const has_cancer = document.querySelector('#cancer').value.trim();
-  const has_asthma = document.querySelector('#asthma').specified
+  const has_asthma = document.querySelector('#asthma').value.trim();
   // const has_cardiac_disease = document.querySelector('#cardiac-disease').value.trim();
   // const has_diabetes = document.querySelector('#diabetes').value.trim();
   // const has_hypertension = document.querySelector('#hypertension').value.trim();
@@ -11,28 +11,28 @@ const historyFormHandler = async (event) => {
 
 
   // if () {
-    const response = await fetch('/api/history/forms', {
-      method: 'POST',
-      body: JSON.stringify({
-        has_cancer,
-        has_asthma
-        // has_cardiac_disease,
-        // has_diabetes,
-        // has_hypertension,
-        // has_psychiatric_disorder,
-        // has_epilepsy
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-    if (response.ok) {
-      document.location.replace('/');
-      console.log(response);
-    } else {
-      alert('Failed to add patient history.');
-    }
+  const response = await fetch('/api/history/forms', {
+    method: 'POST',
+    body: JSON.stringify({
+      has_cancer,
+      has_asthma
+      // has_cardiac_disease,
+      // has_diabetes,
+      // has_hypertension,
+      // has_psychiatric_disorder,
+      // has_epilepsy
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  if (response.ok) {
+    document.location.replace('/');
+    console.log(response);
+  } else {
+    alert('Failed to add patient history.');
   }
+}
 // };
 
 document
