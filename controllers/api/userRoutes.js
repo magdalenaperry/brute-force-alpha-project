7c75d50501a428c49f216d32f7ff9fef8dd240e8
Michael Hoyle <mcalebhoyle@gmail.com>
2022-04-08 11:20:21 -0400

    fixed login buttons on every page that isn't the home page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { User } = require('../../models');
 const serialize = require('../../utils/serialize');
 const Patient = require('../../models/patient')
 
